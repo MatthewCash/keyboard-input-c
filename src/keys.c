ff80773f60a46afd534b354b7336d54b559716b8
Matthew_Cash <matthew@matthew-cash.com>
2022-05-25 13:18:27 -0700

    Use KEY_* definitions for keycodes

--- src/keys.c
+++ src/keys.c
@@ -2,56 +2,57 @@
 
 #include "devices.h"
 #include "http.h"
+#include <linux/input-event-codes.h>
 
 void handle_keycode(const unsigned short key_code)
 {
     switch (key_code) {
-    case 57: // Space bar (toggle scene)
+    case KEY_SPACE: // toggle scene
         send_message("{\"setScene\":null}");
         break;
 
     // Numpad 1-9 colors
-    case 79:
+    case KEY_KP1:
         set_color(0);
         break;
-    case 80:
+    case KEY_KP2:
         set_color(40);
         break;
-    case 81:
+    case KEY_KP3:
         set_color(80);
         break;
-    case 75:
+    case KEY_KP4:
         set_color(120);
         break;
-    case 76:
+    case KEY_KP5:
         set_color(160);
         break;
-    case 77:
+    case KEY_KP6:
         set_color(200);
         break;
-    case 71:
+    case KEY_KP7:
         set_color(240);
         break;
-    case 72:
+    case KEY_KP8:
         set_color(280);
         break;
-    case 73:
+    case KEY_KP9:
         set_color(320);
         break;
 
-    case 82: // Numbad 0 (warm)
+    case KEY_KP0: // warm
         set_lighting("{\"colorTemp\":\"warm\",\"power\":true}");
         break;
-    case 83: // Numpad . (cold)
+    case KEY_KPDOT: // cold
         set_lighting("{\"colorTemp\":\"cold\",\"power\":true}");
         break;
-    case 96: // Numpad enter (off)
+    case KEY_KPENTER: // off
         set_lighting("{\"power\":false}");
         break;
-    case 74: // Numpad minus (brightness down)
+    case KEY_KPMINUS: // brightness down
         set_lighting("{\"adjustBrightness\":-20,\"power\":true}");
         break;
-    case 78: // Numpad plus (brightness up)
+    case KEY_KPPLUS: // brightness up
         set_lighting("{\"adjustBrightness\":20,\"power\":true}");
         break;
     default:
